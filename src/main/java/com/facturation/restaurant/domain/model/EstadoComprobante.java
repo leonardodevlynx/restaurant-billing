f87e2a49e2e4a022168bf7e187096957b63b301f
leonardodevlynx <leonardodevlince@gmail.com>
2026-06-10 14:58:49 -0500

    feature/model-estado-comprobante

---
+++ src/main/java/com/facturation/restaurant/domain/model/EstadoComprobante.java
@@ -0,0 +1,10 @@
+package com.facturation.restaurant.domain.model;
+
+public enum EstadoComprobante {
+    PENDIENTE_ENVIO,
+    ENVIADO,
+    ACEPTADO,
+    RECHAZADO,
+    EXCEPCION,
+    GUARDADO_OFFLINE
+}
